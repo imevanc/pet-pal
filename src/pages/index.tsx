a3233e9038cbd4e8ac085606d8b16ffbf4503536
imevanc <evan.charalampidis@gmail.com>
2022-12-10 21:05:06 +0000

    responsive first layer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,35 @@
 import type { NextPage } from "next";
+import Image from "next/image";
 import React from "react";
 
 const Home: NextPage = () => {
   return (
     <React.Fragment>
-      <main className="flex items-center justify-center bg-white px-6 py-6">
-        <div className="max-w-5xl w-full flex items-center justify-between">
-          <div className="mx-auto max-w-prose text-lg">
+      <main className="h-screen flex flex-col md:flex-row items-center md:items-start md:justify-center bg-white px-6">
+        <div className="max-w-3xl w-full md:pt-10">
+          <div className="mx-auto max-w-prose">
             <h1>
-              <span className="mt-2 block text-center text-3xl font-bold leading-8 tracking-tight text-gray-900 sm:text-4xl">
-                All about dogs in one place
+              <span className="mt-2 block text-left text-3xl font-bold leading-8 tracking-tight text-gray-900 sm:text-4xl lg:text-5xl">
+                All about dogs
+                <br />
+                <span className="text-lime-600">in one place</span>
               </span>
             </h1>
-            <p className="mt-8 text-xl text-center leading-8 text-gray-500">
+            <p className="mt-8 text-lg text-left leading-8 text-gray-500 sm:text-xl lg:text-2xl">
               Our mission is to connect the different parts of the dog industry
               in one place. People and organisations can choose between our
               account types and have instant communication with other users.
             </p>
           </div>
+        </div>
+        <div className="rounded-full shadow-2xl pt-10 md:pt-10">
+          <Image
+            className="rounded-full shadow-2xl"
+            width={320}
+            height={320}
+            src="/lab-puppy.jpg"
+            alt="lab"
+          />
         </div>
       </main>
 
