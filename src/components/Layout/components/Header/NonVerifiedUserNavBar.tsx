ea678dcc4ecf0fa0f7fef634bf3efee22d9229dd
imevanc <evan.charalampidis@gmail.com>
2022-12-14 22:14:05 +0000

    added non verified user components

---
+++ src/components/Layout/components/Header/NonVerifiedUserNavBar.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const NonVerifiedUserNavBar: React.FC = () => {
+  return (
+    <div className="hidden sm:ml-6 sm:flex sm:space-x-8">
+      <a
+        href="#"
+        className="inline-flex items-center border-b-2 border-lime-500 px-1 pt-1 text-lg font-medium text-gray-900"
+      >
+        Home
+      </a>
+      <a
+        href="#"
+        className="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-lg font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
+      >
+        Team
+      </a>
+      <a
+        href="#"
+        className="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-lg font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
+      >
+        Announcements
+      </a>
+    </div>
+  );
+};
+
+export default NonVerifiedUserNavBar;
