caeaf3006437bc543cf14f6a9685935c42ce123f
imevanc <evan.charalampidis@gmail.com>
2022-12-17 16:51:54 +0000

    created a draft 404 template

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,3 +1,28 @@
 export default function Custom404() {
-  return <h1>404 - Page Not Found</h1>;
+  return (
+    <div className="flex min-h-full flex-col bg-white pt-16 pb-12">
+      <div className="mx-auto flex w-full max-w-7xl flex-grow flex-col justify-center px-4 sm:px-6 lg:px-8">
+        <div className="py-16">
+          <div className="text-center">
+            <p className="text-base font-semibold text-indigo-600">404</p>
+            <h1 className="mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">
+              Page not found.
+            </h1>
+            <p className="mt-2 text-base text-gray-500">
+              Sorry, we couldn’t find the page you’re looking for.
+            </p>
+            <div className="mt-6">
+              <a
+                href="#"
+                className="text-base font-medium text-indigo-600 hover:text-indigo-500"
+              >
+                Go back home
+                <span aria-hidden="true"> &rarr;</span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
