ffbdc93243b70685198d22d6c67d9c636e229e6a
imevanc <evan.charalampidis@gmail.com>
2022-12-17 16:59:09 +0000

    custom 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,21 +4,22 @@ export default function Custom404() {
       <div className="mx-auto flex w-full max-w-7xl flex-grow flex-col justify-center px-4 sm:px-6 lg:px-8">
         <div className="py-16">
           <div className="text-center">
-            <p className="text-base font-semibold text-indigo-600">404</p>
-            <h1 className="mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">
-              Page not found.
+            <p className="text-4xl font-sans font-bold text-lime-600 text-8xl">
+              404
+            </p>
+            <h1 className="mt-4 font-bold tracking-tight text-gray-900 sm:text-5xl">
+              Page Not Found
             </h1>
             <p className="mt-2 text-base text-gray-500">
               Sorry, we couldn’t find the page you’re looking for.
             </p>
             <div className="mt-6">
-              <a
-                href="#"
-                className="text-base font-medium text-indigo-600 hover:text-indigo-500"
+              <button
+                type="button"
+                className="inline-flex items-center rounded-md border border-transparent bg-lime-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-lime-700 focus:outline-none focus:ring-2 focus:ring-lime-500 focus:ring-offset-2"
               >
-                Go back home
-                <span aria-hidden="true"> &rarr;</span>
-              </a>
+                Go Back Home
+              </button>
             </div>
           </div>
         </div>
