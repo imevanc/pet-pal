de9b9248f12f0eed20ee36865a57bc2e698e9ef2
imevanc <evan.charalampidis@gmail.com>
2022-12-29 14:11:19 +0000

    integrated the interfaces into the dog owner

--- src/pages/services/dog-owner.tsx
+++ src/pages/services/dog-owner.tsx
@@ -1,33 +1,10 @@
 import type { NextPage } from "next";
 import ServicesPage from "../../components/ServicesPages/ServicesPage";
-
-interface ImageContext {
-  source: string;
-  alternative: string;
-}
-
-interface ConnectionsContext {
-  connectionOne: string;
-  connectionTwo: string;
-  connectionThree: string;
-}
+import dogOwner from "../../data/fixtures/dogOwner.json";
+import { ServicesPageIF } from "../../interfaces/ServicesPageIF";
 
 const DogOwner: NextPage = () => {
-  const heading: string = "Socialise, Learn & Become A Responsible Dog Owner.";
-  const paragraph: string = `Does your dog react unsual and you do not know what to do? We
-  have been there! Use our platform to connect with dog trainers
-  that can help you with different dog issues. Do you like group
-  dog walks or want to avoid some local dogs? You can use our
-  platform and connect with other dog owners globally.`;
-  const connectWith: ConnectionsContext = {
-    connectionOne: "Dog Trainers",
-    connectionTwo: "Food Suppliers",
-    connectionThree: "Dog Walkers",
-  };
-  const image: ImageContext = {
-    source: "/dog-owner.jpg",
-    alternative: "dog-owner-photo",
-  };
+  const { heading, paragraph, connectWith, image }: ServicesPageIF = dogOwner;
   return (
     <ServicesPage
       heading={heading}
