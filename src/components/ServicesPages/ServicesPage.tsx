cce5bfb8c3f46fb044596b8296ebf3b7cf5124e0
imevanc <evan.charalampidis@gmail.com>
2023-01-06 23:39:45 +0000

    floating footer in services page resolved

--- src/components/ServicesPages/ServicesPage.tsx
+++ src/components/ServicesPages/ServicesPage.tsx
@@ -11,17 +11,19 @@ const ServicesPage: React.FC<ServicesPageIF> = ({
   image,
 }) => {
   return (
-    <div className="w-full mt-6 lg:mt-0">
-      <div className="mx-auto max-w-7xl h-[80%] lg:flex lg:justify-center">
-        <div className="flex flex-col lg:flex-row justify-center items-center">
-          <div className="px-4 sm:px-6 text-center lg:text-left md:max-w-2xl lg:flex lg:text-left">
-            <div>
-              <Heading heading={heading} />
-              <Paragraph paragraph={paragraph} />
-              <ConnectWith connectWith={connectWith} />
+    <div className="grow shrink-0">
+      <div className="w-full mt-6 lg:mt-0">
+        <div className="mx-auto max-w-7xl h-[80%] lg:flex lg:justify-center">
+          <div className="flex flex-col lg:flex-row justify-center items-center">
+            <div className="px-4 sm:px-6 text-center lg:text-left md:max-w-2xl lg:flex lg:text-left">
+              <div>
+                <Heading heading={heading} />
+                <Paragraph paragraph={paragraph} />
+                <ConnectWith connectWith={connectWith} />
+              </div>
             </div>
+            <Image image={image} />
           </div>
-          <Image image={image} />
         </div>
       </div>
     </div>
