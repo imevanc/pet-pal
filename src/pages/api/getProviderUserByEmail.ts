5fff405640e684368d975010bb419a132e99294b
imevanc <evan.charalampidis@gmail.com>
2023-01-19 23:29:48 +0000

    created the post db call

--- src/pages/api/getProviderUserByEmail.ts
+++ src/pages/api/getProviderUserByEmail.ts
@@ -6,15 +6,14 @@ const handler = async (
   req: NextApiRequest,
   res: NextApiResponse<User | null>
 ): Promise<any> => {
-  const { email } = req.body;
-  console.log({ email });
+  const { email } = req.query;
   const transformedEmail: string | undefined = email?.slice(1, -1).toString();
 
   try {
-    // const user: User | null = await prisma.user.findUnique({
-    //   where: { email: transformedEmail },
-    // });
-    res.status(200).json(null);
+    const user: User | null = await prisma.user.findUnique({
+      where: { email: transformedEmail },
+    });
+    res.status(200).json(user);
   } catch (error) {
     res.status(400).json(null);
   }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,27 +7,44 @@ import { useSession } from "next-auth/react";
 import React from "react";
 import axios from "axios";
 import { useUserData } from "../hooks/useUserData";
+import { useUser } from "../../lib/hooks";
 
 const Home: NextPage = () => {
   const { data: session } = useSession();
   const { setUser } = useUserData();
+  const user = useUser();
 
   React.useEffect((): void => {
+    const fetchProviderUserByEmail = async (email: string): Promise<any> => {
+      try {
+        const fetchedUser = await axios.get(
+          `/api/getProviderUserByEmail?email=${email}`
+        );
+        if (fetchedUser.data) {
+          setUser(fetchedUser.data);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
     const fetchUserByEmail = async (email: string): Promise<any> => {
       try {
-        const user = await axios.get(`/api/getUserByEmail?email=${email}`);
-        if (user.data) {
-          setUser(user.data);
+        const fetchedUser = await axios.post(`/api/getUserByEmail`, email);
+        if (fetchedUser.data) {
+          setUser(fetchedUser.data);
         }
       } catch (error) {
         console.log(error);
       }
     };
     if (session) {
-      fetchUserByEmail(JSON.stringify(session.user?.email));
+      fetchProviderUserByEmail(JSON.stringify(session.user?.email));
     }
-  }, [session, setUser]);
-
+    if (user?.email) {
+      fetchUserByEmail(user.email);
+    }
+  }, [session, setUser, user]);
+  console.log(user);
   return (
     <div className="flex flex-col justify-between bg-white px-6">
       <DescriptionContainer />
