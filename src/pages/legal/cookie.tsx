24401a53446f709636898dfc65283aa5928d65f2
imevanc <evan.charalampidis@gmail.com>
2023-01-26 23:04:52 +0000

    completed the cookie page

---
+++ src/pages/legal/cookie.tsx
@@ -0,0 +1,8 @@
+import ContentSection from "../../components/ContentSection/ContentSection";
+import type { NextPage } from "next";
+
+const Cookie: NextPage = () => {
+  return <ContentSection content={"cookie"} />;
+};
+
+export default Cookie;
