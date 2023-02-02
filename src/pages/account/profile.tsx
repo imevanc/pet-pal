27e84c83758d78b4ce1fb3ae52697e3f6f5041c4
imevanc <evan.charalampidis@gmail.com>
2023-02-02 19:09:06 +0000

    fixes the production bug

--- src/pages/account/profile.tsx
+++ src/pages/account/profile.tsx
@@ -1,10 +1,7 @@
 import type { NextPage } from "next";
 import ProfilePage from "../../components/ProfilePage/ProfilePage";
-import Cookies from "js-cookie";
 
 const Profile: NextPage = () => {
-  const userCookie = JSON.parse(`${Cookies.get("user")}`);
-  console.log(userCookie);
   return <ProfilePage />;
 };
 
