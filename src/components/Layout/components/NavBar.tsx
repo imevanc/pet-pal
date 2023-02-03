f9f57a7eed2e1a2dfd7a83f6cacc5d75e365f328
imevanc <evan.charalampidis@gmail.com>
2023-02-03 18:30:41 +0000

    fixed the navbar styling

--- src/components/Layout/components/NavBar.tsx
+++ src/components/Layout/components/NavBar.tsx
@@ -1,31 +1,56 @@
 import Link from "next/link";
+import React from "react";
+import { useRouter } from "next/router";
+import { PathsStateIF } from "../../../interfaces/PathsStateIF";
 
 const NavBar: React.FC = () => {
+  const path: string = useRouter().pathname;
+  const [paths, setPaths] = React.useState<PathsStateIF>({
+    dashboard: "",
+    appointments: "",
+  });
+
+  React.useEffect((): void => {
+    if (path === "/account/dashboard") {
+      setPaths({
+        dashboard: "selected",
+        appointments: "nonSelected",
+      });
+    } else if (path === "/account/appointments") {
+      setPaths({
+        dashboard: "nonSelected",
+        appointments: "selected",
+      });
+    } else {
+      setPaths({
+        dashboard: "nonSelected",
+        appointments: "nonSelected",
+      });
+    }
+  }, [path]);
+
+  const selectedClass: string =
+    "inline-flex items-center border-b-2 border-lime-500 px-1 pt-1 text-lg font-medium text-gray-900";
+  const nonSelectedClass: string =
+    "inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-lg font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700";
+
   return (
     <div className="hidden sm:ml-6 sm:flex sm:space-x-8">
       <Link
+        className={
+          paths.dashboard === "selected" ? selectedClass : nonSelectedClass
+        }
         href="/account/dashboard"
-        className="inline-flex items-center border-b-2 border-lime-500 px-1 pt-1 text-lg font-medium text-gray-900"
       >
         Dashboard
       </Link>
       <Link
-        href="/"
-        className="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-lg font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
+        className={
+          paths.announcements === "selected" ? selectedClass : nonSelectedClass
+        }
+        href="/account/announcements"
       >
-        Team
-      </Link>
-      <Link
-        href="/"
-        className="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-lg font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
-      >
-        Projects
-      </Link>
-      <Link
-        href="/"
-        className="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-lg font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
-      >
-        Calendar
+        Announcements
       </Link>
     </div>
   );
