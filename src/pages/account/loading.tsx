1ba2bbca4a9cac36bf5c0a66cdca04c69b18222e
imevanc <evan.charalampidis@gmail.com>
2023-02-04 23:59:00 +0000

    completed the loading page

--- src/pages/account/loading.tsx
+++ src/pages/account/loading.tsx
@@ -1,7 +1,21 @@
 import type { NextPage } from "next";
+import { useRouter } from "next/router";
+import { useUserContext } from "../../hooks/useUserContext";
 
 const Loading: NextPage = () => {
-  return <div>Loading...</div>;
+  const user = useUserContext();
+  const router = useRouter();
+  if (user) {
+    router.replace(`/account/${user?.role}/${user?.id}/dashboard`);
+  }
+  return (
+    <div className="fixed top-0 left-0 right-0 bottom-0 w-full h-screen z-50 overflow-hidden bg-gray-400 opacity-85 flex flex-col items-center justify-center">
+      <div className="loader ease-linear rounded-full border-4 border-t-4 border-gray-200 h-12 w-12 mb-4"></div>
+      <h2 className="text-center text-white text-xl font-semibold">
+        Loading...
+      </h2>
+    </div>
+  );
 };
 
 export default Loading;
