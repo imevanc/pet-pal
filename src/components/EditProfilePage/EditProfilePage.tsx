4ff65690d7fc403e9fc57d72b6d7b9fcfbf30637
imevanc <evan.charalampidis@gmail.com>
2023-02-08 19:02:06 +0000

    created an EditProfilePage component

---
+++ src/components/EditProfilePage/EditProfilePage.tsx
@@ -0,0 +1,11 @@
+const EditProfilePage: React.FC = () => {
+  return (
+    <div className="min-h-full">
+      <div className="py-4 smg:py-10 mx-4">
+        <div className="mx-auto max-w-3xl sm:px-6 md:flex md:items-center md:justify-between md:space-x-5 lg:max-w-7xl lg:px-8"></div>
+      </div>
+    </div>
+  );
+};
+
+export default EditProfilePage;
