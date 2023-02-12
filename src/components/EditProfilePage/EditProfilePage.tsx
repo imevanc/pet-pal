fabe98fb3b1f4cf406edefb2e11e4dbc8a6b872b
imevanc <evan.charalampidis@gmail.com>
2023-02-12 16:52:30 +0000

    create the navigationtabs component and edited the personal information

--- src/components/EditProfilePage/EditProfilePage.tsx
+++ src/components/EditProfilePage/EditProfilePage.tsx
@@ -1,10 +1,7 @@
 import { useRouter } from "next/router";
-import { classNames } from "../../utils/classNames";
 import React from "react";
 import { PathsStateIF } from "../../interfaces/PathsStateIF";
-import { stringToBool } from "../../utils/stringToBool";
-import { ProfileNavigationIF } from "../../interfaces/ProfileNavigationIF";
-import { getProfileNavigation } from "../../utils/getProfileNavigation";
+import NavigationTabs from "./components/NavigationTabs";
 
 const EditProfilePage: React.FC = () => {
   const router = useRouter();
@@ -15,13 +12,6 @@ const EditProfilePage: React.FC = () => {
     settings: "",
   });
 
-  const navigation: ProfileNavigationIF = getProfileNavigation(paths);
-
-  const handleClick = (queryName: string): void => {
-    router.replace({
-      query: { ...router.query, editProfileItem: queryName },
-    });
-  };
   React.useEffect((): void => {
     if (router.query.editProfileItem === "profile") {
       setPaths({
@@ -48,102 +38,40 @@ const EditProfilePage: React.FC = () => {
     <div className="sm:my-4 mx-4">
       <div className="sm:mx-auto max-w-3xl lg:max-w-7xl bg-gray-100">
         <div className="lg:grid lg:grid-cols-12 lg:gap-x-5">
-          <aside className="py-6 px-2 sm:px-6 lg:col-span-3 lg:py-0 lg:px-0">
-            <nav className="space-y-1">
-              {navigation.navigation.map((item) => {
-                return (
-                  <button
-                    key={item.name}
-                    onClick={() => handleClick(item.queryName)}
-                    className={classNames(
-                      stringToBool(item.current)
-                        ? "bg-gray-50 text-lime-700 hover:text-lime-700 hover:bg-white"
-                        : "text-gray-900 hover:text-gray-900 hover:bg-gray-50",
-                      "group rounded-md px-3 py-2 flex items-center text-lg font-medium"
-                    )}
-                    aria-current={item.current ? "page" : undefined}
-                  >
-                    <item.icon
-                      className={classNames(
-                        item.current
-                          ? "text-lime-500 group-hover:text-lime-500"
-                          : "text-gray-400 group-hover:text-gray-500",
-                        "flex-shrink-0 -ml-1 mr-3 h-6 w-6"
-                      )}
-                      aria-hidden="true"
-                    />
-                    <span className="truncate">{item.name}</span>
-                  </button>
-                );
-              })}
-            </nav>
-          </aside>
-
-          <div className="space-y-6 sm:px-6 lg:col-span-9 lg:px-0">
+          <NavigationTabs paths={paths} />
+          <div className="space-y-6 sm:px-6 lg:col-span-9 lg:px-0 m-2">
             <form action="#" method="POST">
-              <div className="shadow-xl sm:overflow-hidden sm:rounded-md">
-                <div className="space-y-6 bg-white py-6 px-4 sm:p-6">
+              <div className="shadow sm:overflow-hidden sm:rounded-md">
+                <div className="bg-white py-6 px-4 sm:p-6">
                   <div>
-                    <h3 className="text-lg font-medium leading-6 text-gray-900">
-                      Profile
+                    <h3 className="text-xl font-medium leading-6 text-gray-900">
+                      Personal Information
                     </h3>
-                    <p className="mt-1 text-sm text-gray-500">
-                      This information will be displayed publicly so be careful
-                      what you share.
-                    </p>
                   </div>
-
-                  <div className="grid grid-cols-3 gap-6">
-                    <div className="col-span-3 sm:col-span-2">
-                      <label
-                        htmlFor="company-website"
-                        className="block text-sm font-medium text-gray-700"
-                      >
-                        Username
-                      </label>
-                      <div className="mt-1 flex rounded-md shadow-sm">
-                        <span className="inline-flex items-center rounded-l-md border border-r-0 border-gray-300 bg-gray-50 px-3 text-gray-500 sm:text-sm">
-                          workcation.com/
-                        </span>
-                        <input
-                          type="text"
-                          name="username"
-                          id="username"
-                          autoComplete="username"
-                          className="block w-full min-w-0 flex-grow rounded-none rounded-r-md border-gray-300 focus:border-lime-500 focus:ring-lime-500 sm:text-sm"
-                        />
-                      </div>
-                    </div>
-
+                  <div className="mt-4 grid grid-cols-6 gap-6">
                     <div className="col-span-3">
                       <label
-                        htmlFor="about"
+                        htmlFor="status"
                         className="block text-sm font-medium text-gray-700"
                       >
-                        About
+                        Status
                       </label>
-                      <div className="mt-1">
-                        <textarea
-                          id="about"
-                          name="about"
-                          rows={3}
-                          className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-lime-500 focus:ring-lime-500 sm:text-sm"
-                          placeholder="you@example.com"
-                          defaultValue={""}
-                        />
-                      </div>
-                      <p className="mt-2 text-sm text-gray-500">
-                        Brief description for your profile. URLs are
-                        hyperlinked.
-                      </p>
+                      <input
+                        type="text"
+                        name="status"
+                        id="status"
+                        autoComplete="status"
+                        placeholder="Status"
+                        className="mt-1 block w-full rounded-md border border-gray-300 py-2 px-3 shadow-sm focus:border-lime-500 focus:outline-none focus:ring-lime-500 sm:text-sm"
+                      />
                     </div>
 
-                    <div className="col-span-3">
+                    <div className="col-span-4">
                       <label className="block text-sm font-medium text-gray-700">
                         Photo
                       </label>
                       <div className="mt-1 flex items-center">
-                        <span className="inline-block h-12 w-12 overflow-hidden rounded-full bg-gray-100">
+                        <span className="inline-block h-14 w-14 overflow-hidden rounded-full bg-gray-100">
                           <svg
                             className="h-full w-full text-gray-300"
                             fill="currentColor"
@@ -152,82 +80,20 @@ const EditProfilePage: React.FC = () => {
                             <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
                           </svg>
                         </span>
-                        <button
-                          type="button"
-                          className="ml-5 rounded-md border border-gray-300 bg-white py-2 px-3 text-sm font-medium leading-4 text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-lime-500 focus:ring-offset-2"
+                        <label
+                          htmlFor="file-upload"
+                          className="cursor-pointer ml-5 rounded-md border border-gray-300 bg-white py-2 px-3 text-sm font-medium leading-4 text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-lime-500 focus:ring-offset-2"
                         >
-                          Change
-                        </button>
+                          <span>Upload</span>
+                          <input
+                            id="file-upload"
+                            name="file-upload"
+                            type="file"
+                            className="sr-only"
+                          />
+                        </label>
                       </div>
                     </div>
-
-                    <div className="col-span-3">
-                      <label className="block text-sm font-medium text-gray-700">
-                        Cover photo
-                      </label>
-                      <div className="mt-1 flex justify-center rounded-md border-2 border-dashed border-gray-300 px-6 pt-5 pb-6">
-                        <div className="space-y-1 text-center">
-                          <svg
-                            className="mx-auto h-12 w-12 text-gray-400"
-                            stroke="currentColor"
-                            fill="none"
-                            viewBox="0 0 48 48"
-                            aria-hidden="true"
-                          >
-                            <path
-                              d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
-                              strokeWidth={2}
-                              strokeLinecap="round"
-                              strokeLinejoin="round"
-                            />
-                          </svg>
-                          <div className="flex text-sm text-gray-600">
-                            <label
-                              htmlFor="file-upload"
-                              className="relative cursor-pointer rounded-md bg-white font-medium text-lime-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-lime-500 focus-within:ring-offset-2 hover:text-lime-500"
-                            >
-                              <span>Upload a file</span>
-                              <input
-                                id="file-upload"
-                                name="file-upload"
-                                type="file"
-                                className="sr-only"
-                              />
-                            </label>
-                            <p className="pl-1">or drag and drop</p>
-                          </div>
-                          <p className="text-xs text-gray-500">
-                            PNG, JPG, GIF up to 10MB
-                          </p>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div className="bg-gray-50 px-4 py-3 text-right sm:px-6">
-                  <button
-                    type="submit"
-                    className="inline-flex justify-center rounded-md border border-transparent bg-lime-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-lime-700 focus:outline-none focus:ring-2 focus:ring-lime-500 focus:ring-offset-2"
-                  >
-                    Save
-                  </button>
-                </div>
-              </div>
-            </form>
-
-            <form action="#" method="POST">
-              <div className="shadow sm:overflow-hidden sm:rounded-md">
-                <div className="space-y-6 bg-white py-6 px-4 sm:p-6">
-                  <div>
-                    <h3 className="text-lg font-medium leading-6 text-gray-900">
-                      Personal Information
-                    </h3>
-                    <p className="mt-1 text-sm text-gray-500">
-                      Use a permanent address where you can recieve mail.
-                    </p>
-                  </div>
-
-                  <div className="grid grid-cols-6 gap-6">
                     <div className="col-span-6 sm:col-span-3">
                       <label
                         htmlFor="first-name"
