07181a3f299cf937cce901b917fe9211225e0b6b
imevanc <evan.charalampidis@gmail.com>
2023-02-28 22:33:53 +0000

    created a Loading component and fixed the floating footer bug

---
+++ src/components/Loading.tsx
@@ -0,0 +1,14 @@
+const Loading: React.FC = () => {
+  return (
+    <div className="grow shrink-0">
+      <div className="p-2 w-full h-full z-50 overflow-hidden bg-lime-50 opacity-65 flex flex-col items-center justify-center">
+        <div className="loader ease-linear rounded-full border-4 border-t-4 border-gray-200 h-12 w-12 mb-4"></div>
+        <h2 className="text-center text-xl font-semibold text-black">
+          Loading...
+        </h2>
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
