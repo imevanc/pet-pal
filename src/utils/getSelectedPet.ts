8978f4c4df89cc6d2bf4de947c9360053ffbf6a3
imevanc <evan.charalampidis@gmail.com>
2023-03-24 23:40:03 +0000

    removes any petSelected query, given that there are no pets

--- src/utils/getSelectedPet.ts
+++ src/utils/getSelectedPet.ts
@@ -1,5 +1,8 @@
 import { Pet } from "@prisma/client";
 
-export const getSelectedPet = (pets: Pet[], selectedPet: string): Pet => {
+export const getSelectedPet = (
+  pets: Pet[],
+  selectedPet: string | string[] | undefined
+): Pet => {
   return pets.find((pet) => pet.id === selectedPet) || pets[0];
 };
