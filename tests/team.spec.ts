120549d353071eecad42b0f9d754e9fe796f3101
imevanc <evan.charalampidis@gmail.com>
2023-04-21 23:04:34 +0100

    updates dependencies test

---
+++ tests/team.spec.ts
@@ -0,0 +1,9 @@
+// __tests__/index.test.jsx
+
+import { render, screen } from "@testing-library/react";
+import Team from "../src/pages/team";
+import "@testing-library/jest-dom";
+
+describe("Team", () => {
+  it("renders a heading", () => {});
+});
